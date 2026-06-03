322f4a2b53060b33f602e1ae67249cd307bd548a
namsujin <a25062401@gmail.com>
2026-06-03 10:45:31 +0900

    feat: 404 페이지 추가

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -15,6 +15,7 @@ import PrivacyPage from "@/pages/policy/PrivacyPage";
 import TermsPage from "@/pages/policy/TermsPage";
 import RestaurantPage from "@/pages/restaurant/RestaurantPage";
 import SearchPage from "@/pages/search/SearchPage";
+import NotFoundPage from "@/pages/error/NotFoundPage";
 //import RequireAuth from "@/shared/auth/RequireAuth";
 
 export const router = createBrowserRouter([
@@ -77,6 +78,10 @@ export const router = createBrowserRouter([
         path: "/mypage",
         element:
                   <MyPage />,
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />,
       },
 
 
