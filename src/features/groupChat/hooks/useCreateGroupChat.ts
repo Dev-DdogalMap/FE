9d2f9f9531d3f1bef435854ef417673066637af5
Lee-Yeonjoo <joos0207@naver.com>
2026-06-05 16:03:21 +0900

    fix: rest 오류 수정

--- src/features/groupChat/hooks/useCreateGroupChat.ts
+++ src/features/groupChat/hooks/useCreateGroupChat.ts
@@ -1,7 +1,6 @@
 import { createGroupChat, getPresignedUrl } from "../api/groupChatApi";
 import { useAuth } from "@/shared/auth/AuthContext";
 
-// useCreateGroupChat.ts
 export function useCreateGroupChat() {
   const { accessToken, refreshAccessToken } = useAuth();
   const chatAuth = { accessToken, refreshAccessToken };
@@ -9,23 +8,21 @@ export function useCreateGroupChat() {
   const create = async (data: any) => {
     let imageKey: string | null = null;
 
-    //입력 데이터에 이미지 파일이 있으면
+    // 입력 데이터에 이미지 파일이 있으면
     if (data.imageFile) {
       // 1. presigned URL 발급
       const { presignedUrl, imageKey: key } = await getPresignedUrl(data.imageFile.name, chatAuth);
-
       // 2. S3 업로드
       await fetch(presignedUrl, {
         method: "PUT",
-        body: data.imageFile
+        body: data.imageFile,
       });
-
       imageKey = key;
     }
 
-    const { imageFile, ...rest } = data; // imageFile 제거
-    // 3. 채팅방 생성
-    return createGroupChat({ ...data, imageKey }, chatAuth);
+    // imageFile 제거 후 채팅방 생성
+    const { imageFile, ...rest } = data;
+    return createGroupChat({ ...rest, imageKey }, chatAuth);
   };
 
   return { create };
