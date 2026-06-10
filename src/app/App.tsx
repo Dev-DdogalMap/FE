0408233e2e811f3b54c22350a58fe3f438386f49
namsujin <a25062401@gmail.com>
2026-06-10 11:51:02 +0900

    feat: 스플래시 화면 구현

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,34 @@ import { AuthProvider } from "@/shared/auth/AuthContext";
 import { RouterProvider } from "react-router-dom";
 import { Toaster } from "sonner";
 import { router } from "./router/router";
+import SplashScreen from "@/pages/splash/SplashScreen";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [showSplash, setShowSplash] = useState(() => {
+    return sessionStorage.getItem("hasSeenSplash") !== "true";
+  });
+
+  useEffect(() => {
+    if (!showSplash) return;
+
+    sessionStorage.setItem("hasSeenSplash", "true");
+
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 2500);
+
+    return () => clearTimeout(timer);
+  }, [showSplash]);
+
+  if (showSplash) {
+    return <SplashScreen />;
+  }
+
   return (
     <AuthProvider>
-      <RouterProvider router={router} />
-      <Toaster position="top-center" richColors />
+        <RouterProvider router={router} />
+        <Toaster position="top-center" richColors />
     </AuthProvider>
   );
 }
